5d73971130864ec96072448c09b618d62f8bf5a8
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-16 01:17:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = "I don't do much."
 
 // Append a kitten to the end of the kittens Array
-var kittens ['Milo', 'Otis', 'Garfield'];
+var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destrutivelyAppendKitten(name) {
-  var dAK = name.
+  var dAK = kittens.push(name);
+  return dAK;
 }
